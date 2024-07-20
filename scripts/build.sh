5efd748f1d20d3edb724a13f506d2c1c7c95b9c5
cashtann <switalamaciej5@gmail.com>
2024-07-20 23:29:27 +0200

    added scripts

---
+++ scripts/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../FileExplorer/build
+make
+
+./appFileExplorer
---
+++ scripts/config_cmake.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../FileExplorer
+cmake -B build -DCMAKE_PREFIX_PATH=/home/cashtan/Qt/6.5.3/gcc_64
